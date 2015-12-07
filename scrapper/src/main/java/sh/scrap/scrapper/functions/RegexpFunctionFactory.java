3266a2fcd1c4334a42b6239dc7740b088ea50d4a
Thiago Souza <tcostasouza@gmail.com>
2015-12-07 17:23:04 -0200

    add regexp tests

--- scrapper/src/main/java/sh/scrap/scrapper/functions/RegexpFunctionFactory.java
+++ scrapper/src/main/java/sh/scrap/scrapper/functions/RegexpFunctionFactory.java
@@ -5,20 +5,41 @@ import sh.scrap.scrapper.DataScrapperFunction;
 import sh.scrap.scrapper.DataScrapperFunctionFactory;
 import sh.scrap.scrapper.DataScrapperFunctionLibrary;
 import sh.scrap.scrapper.annotation.Name;
+import sh.scrap.scrapper.annotation.Requires;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Name("regexp")
+@Name("regexp") @Requires("to-string")
 public class RegexpFunctionFactory implements DataScrapperFunctionFactory {
 
     @Override
     public DataScrapperFunction create(String name, DataScrapperFunctionLibrary library, Object... args) {
         Pattern pattern = Pattern.compile((String) args[0],
                 Pattern.CASE_INSENSITIVE | Pattern.DOTALL | Pattern.MULTILINE);
+        String joiner = args.length > 1 ? (String) args[1] : "";
         return context -> subscriber -> subscriber.onSubscribe(new Subscription() {
             @Override
             public void request(long n) {
+                String data = (String) context.data();
+                Matcher matcher = pattern.matcher(data);
+                List<String> result = new ArrayList<>();
+                while (matcher.find()) {
+                    if (matcher.groupCount() > 0) {
+                        StringBuilder joined = new StringBuilder();
+                        for (int i = 1; i <= matcher.groupCount(); i++)
+                            joined.append(i == 1 ? "" : joiner)
+                                    .append(matcher.group(i));
+                        result.add(joined.toString());
+                    }
+                    else
+                        result.add(matcher.group(0));
+                }
 
+                subscriber.onNext(context.withData(result));
+                subscriber.onComplete();
             }
             @Override public void cancel() {}
         });
--- scrapper/src/test/java/sh/scrap/scrapper/functions/RegexpFunctionTest.java
+++ scrapper/src/test/java/sh/scrap/scrapper/functions/RegexpFunctionTest.java
@@ -1,4 +1,38 @@
 package sh.scrap.scrapper.functions;
 
-public class RegexpFunctionTest {
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+public class RegexpFunctionTest extends FunctionsTestBase {
+
+    @Test
+    public void testRegexp_ungrouped() {
+        String text = "1 2 3";
+        testFunction(new RegexpFunctionFactory(),
+                data -> assertThat(data, equalTo(Arrays.asList("1", "2", "3"))),
+                text, "\\d");
+
+    }
+
+    @Test
+    public void testRegexp_grouped() {
+        String text = "1 2 3 1 5 3";
+        testFunction(new RegexpFunctionFactory(),
+                data -> assertThat(data, equalTo(Arrays.asList("2", "5"))),
+                text, "1 (\\d) 3");
+
+    }
+
+    @Test
+    public void testRegexp_joined() {
+        String text = "1 2 3 5 2 4";
+        testFunction(new RegexpFunctionFactory(),
+                data -> assertThat(data, equalTo(Arrays.asList("1:3", "5:4"))),
+                text, "(\\d) 2 (\\d)", ":");
+
+    }
 }
